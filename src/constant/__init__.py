38f42d0601e42fc5ac7e4e94ef84117acded62fb
Sneh Namdev <sneh441@gmail.com>
2026-01-14 06:50:40 +0530

    deployment structure committed

---
+++ src/constant/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "waferfaultdeployment"
+MONGO_DATABASE_NAME = "fault_detection"
+MONGO_COLLECTION_NAME = "waferfault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://sneh441_db_user:namdev123@cluster0.wwvklbs.mongodb.net/?appName=Cluster0"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
